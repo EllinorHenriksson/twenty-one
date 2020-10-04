5f13cc7f4cb0c04dcf819245aba25fe35c8bba17
mtslck <mats.loock@lnu.se>
2020-10-04 10:10:34 +0200

    Initial commit

---
+++ src/Suits.js
@@ -0,0 +1,21 @@
+/**
+ * Module for Suits.
+ *
+ * @module src/Suits
+ * @author Johan Leitet <johan.leitet@lnu.se>
+ * @author Mats Loock <mats.loock@lnu.se>
+ * @version 1.0.0
+ */
+
+/**
+ * Enum for suits.
+ *
+ * @readonly
+ * @enum {string}
+ */
+export const Suits = Object.freeze({
+  CLUBS: '♣',
+  DIAMONDS: '♦',
+  HEARTS: '♥',
+  SPADES: '♠'
+})
---
+++ src/app.js
@@ -0,0 +1,35 @@
+/**
+ * The starting point of the application.
+ *
+ * @author Johan Leitet <johan.leitet@lnu.se>
+ * @author Mats Loock <mats.loock@lnu.se>
+ * @author // TODO: YOUR NAME <YOUR EMAIL>
+ * @version 1.0.0
+ */
+
+// TODO: Replace the code below with your own game logic.
+
+import { Deck } from './Deck.js'
+
+try {
+  // Create 52 playing cards and...
+  const playingCards = Deck.create()
+  console.log(playingCards.join(', '), '\n')
+
+  // ...shuffle them.
+  Deck.shuffle(playingCards)
+  console.log(playingCards.join(', '), '\n')
+
+  // Draw three playing cards, view the remaining playing cards, the drawn playing cards and
+  // then calculate the value of them.
+  // (`value + playingCard` implicitly calls PlayingCard#valueOf to get
+  //  the primitive value of the current PlayingCard object.)
+  const hand = playingCards.splice(0, 3)
+
+  console.log(playingCards.join(', '))
+
+  const value = hand.reduce((value, playingCard) => value + playingCard, 0)
+  console.log(`${hand.join(' ')} (${value})`)
+} catch (e) {
+  console.error(e.message)
+}
